b98e72849e9dd6898ba9e33e9170ccdaa9f30cee
mortekianond <mortekianond@gmail.com>
2025-04-02 20:48:44 +0700

    create Navbar

---
+++ resources/js/Layouts/Frontend.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Header from "@/Components/Header.vue";
+</script>
+<template>
+    <div class="bg-slate-200 dark:bg-slate-900">
+        <!-- HEADER -->
+        <Header />
+        <main class="min-h-screen">
+            <slot />
+        </main>
+        <!-- FOOTER -->
+    </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,47 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+const colors = require("tailwindcss/colors");
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.vue",
     ],
 
     theme: {
+        fontFamily: {
+            primary: "PlayFair Display",
+            body: "work Sans",
+        },
+        container: {
+            padding: {
+                DEFAULT: "1rem",
+                lg: "3rem",
+            },
+        },
         extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            colors: {
+                "light-primary": "#F6F8D5",
+                "light-secondary": "#98D2C0",
+                "light-third": "#4F959D",
+                "light-fourth": "#205781",
+                "light-fifth": "#FFB433",
+                "dark-primary": "#222831",
+                "dark-secondary": "#31363F",
+                "dark-third": "#76ABAE",
+                "dark-fourth": "#EEEEEE",
+                accent: {
+                    DEFAULT: "#FFB433",
+                    hover: "#F6F8D5",
+                },
+                paragraph: "#FFB433",
+                blue: colors.blue,
+                indigo: colors.indigo,
+                green: colors.green,
+                red: colors.red,
             },
         },
     },
